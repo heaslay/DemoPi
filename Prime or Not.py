0a156675cfec2a17943511c7a07779ca5592689b
heaslay <48138060+heaslay@users.noreply.github.com>
2019-03-03 09:44:26 +1300

    Add files via upload

---
+++ Prime or Not.py
@@ -0,0 +1,9 @@
+num = int(input('Put a number here'))
+if num > 1:
+        
+    for i in range (2,num):
+        if num % i == 0:
+            print ("This isn't a prime number")
+            break
+    else:
+         print ("This is a prime number")
